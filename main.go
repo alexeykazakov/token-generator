6852ea9e2b7f251f63a792f03854b83533fe0406
Alexey Kazakov <alkazako@redhat.com>
2018-02-15 11:56:40 -0800

    Use valid timestamps

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
-	uuid "github.com/satori/go.uuid"
+	"github.com/satori/go.uuid"
 )
 
 type User struct {
@@ -40,13 +41,11 @@ type TokenResult struct {
 func main() {
 	keyLoc := flag.String("key", "foo", "private key location")
 	userIDLoc := flag.String("users", "foo", "user UUIDs location")
-	sessionSate := flag.String("session", "foo", "session state")
 	saveTo := flag.String("saveTo", "foo", "File to save the result to")
 	flag.Parse()
 
 	fmt.Printf("Key location: %s\n", *keyLoc)
 	fmt.Printf("User UUIDs location: %s\n", *userIDLoc)
-	fmt.Printf("Session State: %s\n\n", *sessionSate)
 	fmt.Printf("File to save the result to: %s\n\n", *saveTo)
 
 	key, err := ioutil.ReadFile(*keyLoc)
@@ -70,17 +69,18 @@ func main() {
 	tokens := []TokenResult{}
 	for _, id := range ids {
 		if len(id) > 2 {
-			user, err := loadUser(id)
+			userID := strings.TrimSpace(id)
+			user, err := loadUser(userID)
 			if err != nil {
 				panic(err)
 			}
 			indx++
 			fmt.Printf("User %v: %s, %s, %s\n", indx, user.Data.Attributes.Username, user.Data.Attributes.Email, user.Data.Attributes.FullName)
-			token, err := generateToken(privateKey, user, id, *sessionSate)
+			token, err := generateToken(privateKey, user, userID)
 			if err != nil {
 				panic(err)
 			}
-			tokens = append(tokens, TokenResult{UUID: id, Token: token})
+			tokens = append(tokens, TokenResult{UUID: userID, Token: token})
 		}
 	}
 	result.Tokens = tokens
@@ -97,21 +97,23 @@ type Token struct {
 	Data Data `json:"data"`
 }
 
-func generateToken(key *rsa.PrivateKey, user User, userID string, session string) (string, error) {
+func generateToken(key *rsa.PrivateKey, user User, userID string) (string, error) {
 	token := jwt.New(jwt.SigningMethodRS256)
 	token.Header["kid"] = "0lL0vXs9YRVqZMowyw8uNLR_yr0iFaozdQk9rzq2OVU"
 
+	nowTime := time.Now().Unix()
+	in24Hours := nowTime + 24*60*60
 	token.Claims.(jwt.MapClaims)["jti"] = uuid.NewV4().String()
-	token.Claims.(jwt.MapClaims)["exp"] = 1515627761
+	token.Claims.(jwt.MapClaims)["exp"] = in24Hours
 	token.Claims.(jwt.MapClaims)["nbf"] = 0
-	token.Claims.(jwt.MapClaims)["iat"] = 1513035761
+	token.Claims.(jwt.MapClaims)["iat"] = nowTime
 	token.Claims.(jwt.MapClaims)["iss"] = "https://sso.openshift.io/auth/realms/fabric8"
 	token.Claims.(jwt.MapClaims)["aud"] = "fabric8-online-platform"
 	token.Claims.(jwt.MapClaims)["sub"] = userID
 	token.Claims.(jwt.MapClaims)["typ"] = "Bearer"
 	token.Claims.(jwt.MapClaims)["azp"] = "fabric8-online-platform"
-	token.Claims.(jwt.MapClaims)["auth_time"] = 1513035761
-	token.Claims.(jwt.MapClaims)["session_state"] = session
+	token.Claims.(jwt.MapClaims)["auth_time"] = nowTime
+	token.Claims.(jwt.MapClaims)["session_state"] = uuid.NewV4().String()
 	token.Claims.(jwt.MapClaims)["acr"] = "1"
 	token.Claims.(jwt.MapClaims)["approved"] = "true"
 	token.Claims.(jwt.MapClaims)["name"] = user.Data.Attributes.FullName
@@ -121,7 +123,6 @@ func generateToken(key *rsa.PrivateKey, user User, userID string, session string
 	token.Claims.(jwt.MapClaims)["family_name"] = ""
 	token.Claims.(jwt.MapClaims)["email"] = user.Data.Attributes.Email
 
-	//	token.Claims.(jwt.MapClaims)["iat"] = time.Now().Unix()
 	token.Claims.(jwt.MapClaims)["allowed-origins"] = []string{
 		"http://auth.openshift.io",
 		"http://api.openshift.io",
